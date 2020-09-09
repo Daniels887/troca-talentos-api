db74d75b729f0555d8c5940fc4951e8f9c4bc365
Daniel <daniel_sousa.p@outlook.com>
2020-09-09 00:04:22 -0300

    feat: create migration of schedules table

---
+++ src/database/migrations/1599620276796-CreateSchedulesTable.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateSchedulesTable1599620276796 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'schedules',
+      columns: [
+        {
+          name: 'id',
+          type: 'int',
+          isPrimary: true,
+          generationStrategy: 'increment',
+        },
+        {
+          name: 'id_provider',
+          type: 'uuid',
+        },
+        {
+          name: 'id_contractor',
+          type: 'uuid',
+        },
+        {
+          name: 'date',
+          type: 'timestamp',
+        },
+      ],
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('schedules');
+  }
+}
