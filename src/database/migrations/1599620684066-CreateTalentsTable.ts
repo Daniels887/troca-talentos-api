a7682978f198d42ff5d045158737754b88cb07e6
Daniel <daniel_sousa.p@outlook.com>
2020-09-09 00:35:25 -0300

    feat: create migration of talents table

---
+++ src/database/migrations/1599620684066-CreateTalentsTable.ts
@@ -0,0 +1,38 @@
+import {
+  MigrationInterface, QueryRunner, Table, TableForeignKey,
+} from 'typeorm';
+
+export class CreateTalentsTable1599620684066 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'talents',
+      columns: [
+        {
+          name: 'id',
+          type: 'int',
+          isPrimary: true,
+          generationStrategy: 'increment',
+        },
+        {
+          name: 'id_user',
+          type: 'uuid',
+        },
+        {
+          name: 'talent',
+          type: 'varchar',
+        },
+      ],
+    }));
+
+    await queryRunner.createForeignKey('talents', new TableForeignKey({
+      columnNames: ['id_user'],
+      referencedColumnNames: ['id'],
+      referencedTableName: 'users',
+      onDelete: 'CASCADE',
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('talents');
+  }
+}
